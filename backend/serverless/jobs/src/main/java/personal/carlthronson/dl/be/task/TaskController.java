fe6d478a4cc6030f67778f78bff006076830016b
Carl Thronson <carlthronson@gmail.com>
2023-10-15 15:33:23 -0700

    Remove services because they just call through to repository Creating an extra layer that can only introduce errors
    Rebuild the relationship between Story and Task
    So that Story objects contain a list of Tasks
    So that we don't have to query for the tasks for a story

--- backend/serverless/jobs/src/main/java/personal/carlthronson/dl/be/task/TaskController.java
+++ backend/serverless/jobs/src/main/java/personal/carlthronson/dl/be/task/TaskController.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,51 +20,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import personal.carlthronson.dl.be.story.Story;
-import personal.carlthronson.dl.be.story.StoryService;
-
 @RestController
 @EnableWebMvc
 @Transactional
 public class TaskController {
     @Autowired
-    TaskService service;
-
-    @Autowired
-    StoryService storyService;
+    TaskRepository repository;
 
     @RequestMapping(path = "/task", method = RequestMethod.POST)
-    public Task create(@RequestBody Task task) {
-        return service.create(task);
+    public Task save(@RequestBody Task task) {
+        return repository.save(task);
+    }
+
+    @RequestMapping(path = "/task/{id}", method = RequestMethod.GET)
+    public Task findOne(@PathVariable Integer id) {
+        Task task = new Task();
+        task.setId(id);
+        ExampleMatcher matcher = ExampleMatcher.matchingAny().withMatcher("id",
+                ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase());
+        Example<Task> example = Example.of(task, matcher);
+        return repository.findOne(example);
     }
 
     @RequestMapping(path = "/task", method = RequestMethod.GET)
-    public List<Task> list(@RequestParam("limit") Optional<Integer> limit, Principal principal) {
+    public List<Task> findAll(@RequestParam("limit") Optional<Integer> limit, Principal principal) {
         try {
-            return service.list();
-        } catch (Exception ex) {
-            List<Task> list = new ArrayList<>();
-            Task task = new Task();
-            task.setName("exception");
-            OutputStream out = new ByteArrayOutputStream();
-            PrintWriter writer = new PrintWriter(out);
-            for (StackTraceElement element : ex.getStackTrace()) {
-                writer.println(element.toString());
-            }
-            writer.flush();
-            task.setLabel(out.toString());
-            list.add(task);
-            return list;
-        }
-    }
-
-    @RequestMapping(path = "/task/list/{storyId}", method = RequestMethod.GET)
-    public List<Task> list(@PathVariable Integer storyId,
-            @RequestParam("limit") Optional<Integer> limit,
-            Principal principal) {
-        try {
-            Story story = storyService.findById(storyId);
-            return service.list(story);
+            return repository.findAll();
         } catch (Exception ex) {
             List<Task> list = new ArrayList<>();
             Task task = new Task();
