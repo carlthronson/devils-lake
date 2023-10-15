5ddccddf9282db6edf4912ed16430a71762af54b
Carl Thronson <carlthronson@gmail.com>
2023-10-15 16:43:10 -0700

    Make Story and Task ids be generated values

--- backend/serverless/jobs/src/main/java/personal/carlthronson/dl/be/story/Story.java
+++ backend/serverless/jobs/src/main/java/personal/carlthronson/dl/be/story/Story.java
@@ -3,6 +3,8 @@ package personal.carlthronson.dl.be.story;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -15,7 +17,7 @@ import personal.carlthronson.dl.be.task.Task;
 @Entity(name = "story")
 public class Story {
     @Id
-//    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
     @Setter
     private long id;
