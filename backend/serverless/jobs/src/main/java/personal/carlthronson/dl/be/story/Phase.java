0da05f7b1dee5ced6469dcf7517a232cb178caa5
Carl Thronson <carlthronson@gmail.com>
2023-10-19 12:57:40 -0700

    Add relationship between Phase and Status

--- backend/serverless/jobs/src/main/java/personal/carlthronson/dl/be/story/Phase.java
+++ backend/serverless/jobs/src/main/java/personal/carlthronson/dl/be/story/Phase.java
@@ -1,11 +1,18 @@
 package personal.carlthronson.dl.be.story;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import personal.carlthronson.dl.be.task.Status;
 
 @Entity(name="phase")
 public class Phase {
@@ -23,4 +30,11 @@ public class Phase {
     @Getter
     @Setter
     private String label;
+
+    @Getter
+    @Setter
+    @OneToMany
+    @JoinColumn(name = "phase_id", nullable = true, unique = false)
+    @JsonManagedReference
+    private List<Status> statuses;
 }
