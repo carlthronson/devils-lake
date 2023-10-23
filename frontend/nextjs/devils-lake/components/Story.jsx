a3c21dccd2b086e3ea8d9af701db45917a78ee9d
Carl Thronson <carlthronson@gmail.com>
2023-10-23 15:50:49 -0700

    More cleanup of styles and style names

--- frontend/nextjs/devils-lake/components/Story.jsx
+++ frontend/nextjs/devils-lake/components/Story.jsx
@@ -5,13 +5,13 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Task from './Task';
 
-const CompanyHeader = styled.div`
+const StoryLabel = styled.div`
     display: flex;
     justify-content: space-between;
     flex-direction: row;
 `;
 
-const Container = styled.div`
+const StoryArea = styled.div`
     border-radius: 10px;
   box-shadow: 5px 5px 5px 2px grey;
     padding: 8px;
@@ -27,33 +27,19 @@ const Container = styled.div`
     flex-direction: column;
 `   ;
 
-const TextContent = styled.div``;
-
-const Icons = styled.div`
-    display: flex;
-    justify-content: end;
-    padding: 2px;
-`   ;
-
 export default function Story({ story, statuses, index }) {
   const { getCollapseProps, getToggleProps, isExpanded } = useCollapse()
 
   return (
-    <Container>
-      <CompanyHeader {...getToggleProps()}>
+    <StoryArea>
+      <StoryLabel {...getToggleProps()}>
         <small>{story.label}</small>
-      </CompanyHeader>
+      </StoryLabel>
       <section {...getCollapseProps()}>
-        <div>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'start', padding: 2 }}></div>
-        <p></p>
         {story.tasks.map((item, index) => (
-          <div key={index}>
             <Task key={index} task={item} story={story} statuses={statuses} index={index} />
-          </div>
         ))}
       </section>
-    </Container>
+    </StoryArea>
   );
 }
