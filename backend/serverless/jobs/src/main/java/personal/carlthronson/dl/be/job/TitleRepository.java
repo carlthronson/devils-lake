5defb3c62ea7d357fe2d3da7cd025694e9d47c9a
Carl Thronson <carlthronson@gmail.com>
2023-10-26 12:45:21 -0700

    Rollback Title

--- backend/serverless/jobs/src/main/java/personal/carlthronson/dl/be/job/TitleRepository.java
+++
@@ -1,7 +0,0 @@
-package personal.carlthronson.dl.be.job;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TitleRepository extends JpaRepository<Title, Long> {
-
-}
