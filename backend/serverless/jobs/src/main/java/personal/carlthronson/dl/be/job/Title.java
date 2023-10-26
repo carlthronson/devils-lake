331c14849dbb33b96ec5fc3a8de6811171e31075
Carl Thronson <carlthronson@gmail.com>
2023-10-26 12:40:14 -0700

    Add join column meta data to title

--- backend/serverless/jobs/src/main/java/personal/carlthronson/dl/be/job/Title.java
+++ backend/serverless/jobs/src/main/java/personal/carlthronson/dl/be/job/Title.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import lombok.Getter;
 import lombok.Setter;
 import personal.carlthronson.dl.be.task.Status;
@@ -18,5 +19,6 @@ public class Title {
     private long id;
 
     @Getter @Setter String name;
+    @JoinColumn(name = "status_id", nullable = false, unique = false)
     @Getter @Setter Status status;
 }
