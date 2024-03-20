610915db4fd026f50daf872508168b7dd5fcf247
Carl Thronson <carlthronson@gmail.com>
2024-03-20 16:13:17 -0700

    Change the title of the main page

--- frontend/nextjs/devils-lake/app/layout.jsx
+++ frontend/nextjs/devils-lake/app/layout.jsx
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head><title>Minimal runnable nextjs app</title></head>
+      <head><title>Carl's App</title></head>
       <body>{children}</body>
     </html>
   )
