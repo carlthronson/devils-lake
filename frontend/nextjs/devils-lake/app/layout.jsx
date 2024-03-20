90e3b61959c4b0c608290f947011a1394bf5bdb5
Carl Thronson <carlthronson@gmail.com>
2024-03-20 16:23:59 -0700

    Force another build on vercel

--- frontend/nextjs/devils-lake/app/layout.jsx
+++ frontend/nextjs/devils-lake/app/layout.jsx
@@ -1,9 +1,8 @@
 export default function RootLayout({ children }) {
-  let title = "Carls App";
 
   return (
     <html lang="en">
-      <head><title>{title}</title></head>
+      <head><title>Carls App</title></head>
       <body>{children}</body>
     </html>
   )
