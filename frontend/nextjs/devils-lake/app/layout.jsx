03fdcd1bc1f605058ebe265dbe95132f536d8c64
Carl Thronson <carlthronson@gmail.com>
2024-03-20 16:27:11 -0700

    Change the title again!!

--- frontend/nextjs/devils-lake/app/layout.jsx
+++ frontend/nextjs/devils-lake/app/layout.jsx
@@ -2,7 +2,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <head><title>Carls App</title></head>
+      <head><title>Something Completely Different</title></head>
       <body>{children}</body>
     </html>
   )
