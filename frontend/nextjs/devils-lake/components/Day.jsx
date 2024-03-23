4a9890ab4e77ecff17cdb21b774107388c987183
Carl Thronson <carlthronson@gmail.com>
2024-03-22 20:42:30 -0700

    Use useEffect

--- frontend/nextjs/devils-lake/components/Day.jsx
+++ frontend/nextjs/devils-lake/components/Day.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import './style.css';
 import moment from 'moment';
@@ -18,17 +19,10 @@ let getNotes = function(day) {
 
 export default function Day({ day, backgroundColor, index }) {
     console.log(`Index: ${index} background-color: ${backgroundColor}`);
-    // console.log(`Day: ${day.date()}`);
-    // console.log(`Compare to: ${moment().local().date()}`);
-    // let today = day.date() == currentDay;
+    useEffect(() => {
+      });
 
-    // console.log(`Day: ${day.date()}`);
-    // console.log(`Is this day the current day: ${today}`);
-    // let className = today ? 'today day' : 'day';
-    // console.log(`Class name: ${className}`);
-    // let style = today ? {'background-color':'yellow'} : {};
-
-    let style = {'backgroundColor':backgroundColor};
+      let style = {'backgroundColor':backgroundColor};
 
     return <div style={style} className={'day'}>
         <div className='date'>{day.format('MMM D')}</div>
