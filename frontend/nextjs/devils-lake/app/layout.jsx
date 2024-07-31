f4d71298ec5343f3ccaba165e947b159eb7593fa
Carl Thronson <carlthronson@gmail.com>
2024-07-31 16:14:48 -0700

    Change the page title

--- frontend/nextjs/devils-lake/app/layout.jsx
+++ frontend/nextjs/devils-lake/app/layout.jsx
@@ -2,7 +2,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <head><title>{"Carl's Calendar"}</title></head>
+      <head><title>{"Carl's Application"}</title></head>
       <body>{children}</body>
     </html>
   )
