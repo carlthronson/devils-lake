165af11e032d42fdfe7e63a81a92336dd81e14cb
Carl Thronson <carlthronson@gmail.com>
2024-10-01 15:17:06 -0700

    Redirect to render

--- frontend/nextjs/devils-lake/next.config.js
+++ frontend/nextjs/devils-lake/next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://zve6rqyqh8.execute-api.us-west-1.amazonaws.com/dev/:path*' // Proxy to Backend
+        destination: 'https://devils-lake-backend.onrender.com/dev/:path*' // Proxy to Backend
       }
     ]
   },
